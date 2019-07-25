0e17ccd169aab4e129da8400477653a864ef1056
Nuno <ngocalves@gmail.com>
2019-07-25 23:12:45 +0200

    configureStore: enable loggerMiddleware + IE compatible

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,17 +2,20 @@ import { applyMiddleware, compose, createStore } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 
 import monitorReducersEnhancer from './enhancers/monitorReducers'
-// import loggerMiddleware from './middleware/logger'
+import loggerMiddleware from './middleware/logger'
 import rootReducer from '../js/reducers'
 
 export default function configureStore(preloadedState) {
-  const middlewares = [thunkMiddleware] // loggerMiddleware,
+  const middlewares = [loggerMiddleware, thunkMiddleware]
   const middlewareEnhancer = applyMiddleware(...middlewares)
 
   const enhancers = [
     middlewareEnhancer,
     monitorReducersEnhancer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
   ]
   const composedEnhancers = compose(...enhancers)
 
