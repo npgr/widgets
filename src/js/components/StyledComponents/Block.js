a6a357927092b890ce962a1a9356a8de460889fd
Nuno <ngocalves@gmail.com>
2019-07-30 00:18:39 +0200

    ESLint on Block component

--- src/js/components/StyledComponents/Block.js
+++ src/js/components/StyledComponents/Block.js
@@ -1,28 +1,30 @@
-import styled from "styled-components";
-import PropTypes from "prop-types";
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
 const Block = styled.div`
     ${({ align }) => align && `text-align: ${align};`}
     ${({ bgColor }) => bgColor && `background-color: ${bgColor};`}
-    ${({ flex }) => flex && "display: flex;"}
+    ${({ flex }) => flex && 'display: flex;'}
     ${({ height }) => height && `height: ${height};`}
+    ${({ margin }) => margin && `height: ${margin};`}
     ${({ pright }) => pright && `padding-right: ${pright};`}
     ${({ ptop }) => ptop && `padding-top: ${ptop};`}
-    ${({ spaceBetween }) => spaceBetween && "justify-content: space-between;"}
+    ${({ spaceBetween }) => spaceBetween && 'justify-content: space-between;'}
     ${({ width }) => width && `width: ${width};`}
-    padding: ${({ padding }) => padding || "2vh 2vw"};
-`;
+    padding: ${({ padding }) => padding || '2vh 2vw'};
+`
 
 Block.propTypes = {
   align: PropTypes.string,
   bgColor: PropTypes.string,
   flex: PropTypes.bool,
   height: PropTypes.string,
+  margin: PropTypes.string,
   padding: PropTypes.string,
   pright: PropTypes.string,
   ptop: PropTypes.string,
   spaceBetween: PropTypes.bool,
   width: PropTypes.string
-};
+}
 
-export default Block;
+export default Block
