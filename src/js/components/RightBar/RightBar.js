d6e678437e4aeebe77974b7318f1e34c27c620b6
Nuno <ngocalves@gmail.com>
2019-07-30 21:13:34 +0200

    dependencies for charts and dragAndDrop

--- src/js/components/RightBar/RightBar.js
+++ src/js/components/RightBar/RightBar.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { useDrag } from 'react-dnd'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faChartPie,
+  faChartLine,
+  faChartBar,
+  faChartArea
+} from '@fortawesome/free-solid-svg-icons'
 import { Block } from '../StyledComponents'
 
 export default () => (
   <Block width='25vw' height='90vh' bgColor='gainsboro' ptop='4vh'>
     <Block bgColor='white' height='83vh'>
-      Charts
+      Charts:
+      <Block>
+        <FontAwesomeIcon icon={faChartPie} size='3x' />
+      </Block>
+      <Block>
+        <FontAwesomeIcon icon={faChartLine} size='3x' />
+      </Block>
+      <Block>
+        <FontAwesomeIcon icon={faChartBar} size='3x' />
+      </Block>
+      <Block>
+        <FontAwesomeIcon icon={faChartArea} size='3x' />
+      </Block>
     </Block>
   </Block>
 )
